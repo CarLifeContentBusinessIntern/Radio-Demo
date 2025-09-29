139137746d54bb2060b28df71dff75794e74a06a
JoyKang210 <Joy.Kang@obigo.com>
2025-09-29 17:06:49 +0900

    feat: 리스트 목록 컴포넌트

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop.tsx";
 import Home from "./pages/Home.tsx";
+import ListViewPage from "./pages/ListViewPage.tsx";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/channels/detail" element={<ListViewPage />} />
     </Routes>
   );
 }
