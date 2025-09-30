5407813f88e929d857d2c3b37cad130aceb36d26
Jennifer <jennifer.choi@obigo.com>
2025-09-30 14:06:01 +0900

    style: 긴 텍스트 말줄임표 처리

--- src/components/CircleViewItem.tsx
+++ src/components/CircleViewItem.tsx
@@ -12,10 +12,10 @@ function CircleViewItem({ title, subTitle }: CircleViewItemProps) {
                 <div className="bg-gray-600 w-full aspect-square rounded-full mb-4" />
                 <img src={PickleLogo} className="absolute top-6 inset-x-0 mx-auto" />
             </div>
-            <p className="text-[28px] mb-2 px-1 font-semibold">
+            <p className="text-[28px] mb-2 px-1 font-semibold truncate">
                 {title}
             </p>
-            <p className="text-[25px] text-gray-400 px-1">
+            <p className="text-[25px] text-gray-400 px-1 truncate">
                 {subTitle}
             </p>
         </div>
