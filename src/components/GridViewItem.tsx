bf45d4e4c1b4170177ae1b87615055479cae4103
Jennifer <jennifer.choi@obigo.com>
2025-09-30 10:08:25 +0900

    style: 홈페이지 UI 구현

---
+++ src/components/GridViewItem.tsx
@@ -0,0 +1,21 @@
+interface GridViewItemProps {
+    title?: string;
+    subTitle?: string;
+}
+
+function GridViewItem({ title, subTitle }: GridViewItemProps) {
+    return (
+        <div className="cursor-pointer">
+            <div className="bg-gray-600 w-full aspect-square rounded-[45px] mb-4" />
+            <p className="text-[28px] mb-2 px-1 font-semibold">
+                {title}
+            </p>
+            <p className="text-[25px] text-gray-400 px-1">
+                {subTitle}
+            </p>
+        </div>
+    )
+
+}
+
+export default GridViewItem
