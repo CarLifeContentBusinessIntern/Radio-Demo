545f6af71e051849fe360ccea9fc972491460623
JoyKang210 <Joy.Kang@obigo.com>
2025-10-14 17:32:15 +0900

    feat: 지금은 섹션 데이터 연동

--- src/pages/RadioLiveVersion.tsx
+++ src/pages/RadioLiveVersion.tsx
@@ -4,6 +4,9 @@ import GridViewItem from '../components/GridViewItem';
 import { mockCategoryData } from '../mock/mockCategoryData';
 import { mockChannelData } from '../mock/mockChannelData';
 import { mockEpisodeData } from '../mock/mockEpisodeData';
+import { useEffect, useState } from 'react';
+import { supabase } from '../lib/supabaseClient';
+import type { LiveRadio } from '../types/radio';
 
 function RadioLiveVersion() {
   const navigate = useNavigate();
@@ -20,19 +23,41 @@ function RadioLiveVersion() {
     return mockCategoryData[index % mockCategoryData.length];
   });
 
+  const [liveData, setLiveData] = useState<LiveRadio[]>([]);
+
+  useEffect(() => {
+    async function fetchLiveData() {
+      const { data: liveRadioData, error: liveError } = await supabase
+        .from('radios')
+        .select('*, channels(*)')
+        .eq('is_live', true)
+        .order('live_no', { ascending: true });
+
+      if (liveError) {
+        console.log('❌ Error fetching live data:', liveError.message);
+        return;
+      }
+      setLiveData(liveRadioData);
+    }
+
+    fetchLiveData();
+  }, []);
+
   return (
     <div className="pr-28 pt-7">
       <div className="text-2xl mb-7 font-semibold">지금은?</div>
       <div className="grid gap-x-4 gap-y-7 mb-16 px-1 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {displayEpData.map((item, index) => (
-          <GridViewItem
-            key={`${item.id}-${index}`}
-            title={item.title}
-            subTitle={item.channel}
-            onClick={() => navigate(`/player/${item.id}`)}
-          />
-        ))}
-        <GridViewItem title="더보기" subTitle="더보기" />
+        {liveData &&
+          liveData.map((item, index) => (
+            <GridViewItem
+              key={`${item.id}-${index}`}
+              title={item.title}
+              subTitle={item.channels.broadcasting + item.channels.channel}
+              img={item.img_url}
+              onClick={() => navigate(`/player/${item.id}`)}
+            />
+          ))}
+        {/* <GridViewItem title="더보기" subTitle="더보기" /> */}
       </div>
 
       <div className="text-2xl mb-7 font-semibold">방송사별 라디오</div>
---
+++ src/types/radio.ts
@@ -0,0 +1,21 @@
+import type { Category } from './category';
+import type { Channel } from './channel';
+
+export interface Radio {
+  id: number;
+  title: string;
+  time_slot: string;
+  img_url: string;
+  categories: Category;
+  channels: Channel;
+  is_live: boolean;
+}
+
+export interface LiveRadio {
+  id: number;
+  title: string;
+  is_live: boolean;
+  channel_id: number;
+  channels: Channel;
+  img_url: string;
+}
