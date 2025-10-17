cdb2d5143eb685365737636600cf08bbd0abf562
Jennifer <jennifer.choi@obigo.com>
2025-10-17 09:36:46 +0900

    feat: 방송사 이미지 조건 추가 주석 처리

--- src/components/CircleViewItem.tsx
+++ src/components/CircleViewItem.tsx
@@ -1,4 +1,4 @@
-import { useVersion } from '../contexts/VersionContext';
+// import { useVersion } from '../contexts/VersionContext';
 
 interface CircleViewItemProps {
   isLoading?: boolean;
@@ -9,33 +9,32 @@ interface CircleViewItemProps {
 }
 
 function CircleViewItem({ title, subTitle, img, onClick }: CircleViewItemProps) {
-  const { isLiveVersion } = useVersion();
   const trimedTitle = title?.trimEnd();
-  let finalImg = img;
-  console.log(title);
 
   //방송사 조건 추가
-  if (isLiveVersion) {
-    if (trimedTitle === 'MBC') {
-      finalImg = 'https://radio-web-demo.netlify.app/img/logo/MBC1.png';
-    } else if (trimedTitle === 'SBS') {
-      finalImg = 'https://radio-web-demo.netlify.app/img/logo/SBS1.png';
-    } else if (trimedTitle === 'KBS') {
-      finalImg = 'https://radio-web-demo.netlify.app/img/logo/KBS1.png';
-    }
-  } else {
-    if (trimedTitle === 'MBC') {
-      finalImg = 'https://radio-web-demo.netlify.app/img/logo/MBC2.png';
-    } else if (trimedTitle === 'SBS') {
-      finalImg = 'https://radio-web-demo.netlify.app/img/logo/SBS2.png';
-    } else if (trimedTitle === 'KBS') {
-      finalImg = 'https://radio-web-demo.netlify.app/img/logo/KBS2.png';
-    }
-  }
+  // const { isLiveVersion } = useVersion();
+  // let finalImg = img;
+  // if (isLiveVersion) {
+  //   if (trimedTitle === 'MBC') {
+  //     finalImg = 'https://radio-web-demo.netlify.app/img/logo/MBC1.png';
+  //   } else if (trimedTitle === 'SBS') {
+  //     finalImg = 'https://radio-web-demo.netlify.app/img/logo/SBS1.png';
+  //   } else if (trimedTitle === 'KBS') {
+  //     finalImg = 'https://radio-web-demo.netlify.app/img/logo/KBS1.png';
+  //   }
+  // } else {
+  //   if (trimedTitle === 'MBC') {
+  //     finalImg = 'https://radio-web-demo.netlify.app/img/logo/MBC2.png';
+  //   } else if (trimedTitle === 'SBS') {
+  //     finalImg = 'https://radio-web-demo.netlify.app/img/logo/SBS2.png';
+  //   } else if (trimedTitle === 'KBS') {
+  //     finalImg = 'https://radio-web-demo.netlify.app/img/logo/KBS2.png';
+  //   }
+  // }
   return (
     <div className="cursor-pointer" onClick={onClick}>
       <img
-        src={finalImg}
+        src={img}
         className="w-full aspect-square rounded-full mb-4 flex items-center justify-center overflow-hidden"
       />
       <p className="text-[28px] mb-1 px-1 font-semibold truncate">{trimedTitle}</p>
