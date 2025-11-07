f58be4b0bdcd4a6d0fa5767d2b79c1d6478a9b46
JoyKang210 <Joy.Kang@obigo.com>
2025-11-07 10:26:59 +0900

    refactor: 라디오 믹스 DB 로직 수정

--- src/components/RadioMix.tsx
+++ src/components/RadioMix.tsx
@@ -2,10 +2,49 @@ import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { useSection } from '../hooks/useSection';
 import CircleViewItem from './CircleViewItem';
+import { supabase } from '../lib/supabaseClient';
+import type { SectionItemType } from '../types/section';
 
 function RadioMix() {
   const navigate = useNavigate();
   const { data: sectionData, isLoading } = useSection(6);
+
+  const handleItemClick = async (item: SectionItemType) => {
+    if (item.has_episodes) {
+      try {
+        const table = 'series_episodes';
+        const column = 'series_id';
+
+        const { data, error } = await supabase
+          .from(table)
+          .select('*, episodes(*, programs(*, broadcastings(*)))')
+          .eq(column, item.id)
+          .order('order', { ascending: true });
+
+        if (error) {
+          throw error;
+        }
+
+        const playlist = data ? data.map((joinEntry) => joinEntry.episodes) : [];
+
+        if (playlist.length === 0) {
+          toast.error(`콘텐츠 준비 중입니다`, { toastId: item.id });
+          return;
+        }
+
+        navigate(`/player/${item.first_episode_id}`, {
+          state: { isLive: false, playlist: playlist },
+        });
+      } catch (error) {
+        console.error('플레이리스트 조회 실패:', error);
+        toast.error(`플레이리스트를 불러오는 데 실패했습니다.`, {
+          toastId: 'playlist-fetch-error',
+        });
+      }
+    } else {
+      toast.error(`콘텐츠 준비 중입니다`, { toastId: item.id });
+    }
+  };
 
   return (
     <div>
@@ -25,15 +64,7 @@ function RadioMix() {
                 key={item.id}
                 title={item.title}
                 img={item.img_url}
-                onClick={() => {
-                  if (item.has_episodes) {
-                    navigate(`/player/${item.first_episode_id}`, {
-                      state: { isLive: false, playlist: item, playlistType: 'ThemeType' },
-                    });
-                  } else {
-                    toast.error(`콘텐츠 준비 중입니다`, { toastId: item.id });
-                  }
-                }}
+                onClick={() => handleItemClick(item)}
               />
             ))}
       </div>
