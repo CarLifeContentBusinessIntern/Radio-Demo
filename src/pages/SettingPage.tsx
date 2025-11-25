a005588b227ab0939f8e45cf930d5bdcdcfd40e8
JoyKang210 <Joy.Kang@obigo.com>
2025-11-25 09:20:38 +0900

    refactor: 세팅페이지 스타일 수정 및 주석 제거

--- src/pages/SettingPage.tsx
+++ src/pages/SettingPage.tsx
@@ -3,12 +3,6 @@ import RightArrowIcon from '../assets/rightArrowIcon.svg';
 
 function SettingPage() {
   const navigate = useNavigate();
-  // const { isLiveVersion, toggleLiveVersion, isRadioVersion, toggleRadioVersion } = useVersion();
-  // const [selectedCountry, setSelectedCountry] = useState('대한민국');
-
-  // const handleCountryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-  //   setSelectedCountry(event.target.value);
-  // };
 
   return (
     <div>
@@ -76,78 +70,9 @@ function SettingPage() {
 
         <div className="border-t border-gray-400 border-[1px] w-full" />
 
-        <div className="flex px-4 items-center h-[170px] justify-center text-gray-400 cursor-pointer">
+        <div className="flex px-4 items-center h-[120px] justify-center text-gray-400 cursor-pointer">
           <p className="text-[42px]">서비스 탈퇴</p>
         </div>
-
-        {/* <div className="flex px-4 items-center h-[170px] justify-between">
-          <p className="text-[42px]">국가</p>
-
-          <div className="relative w-60 h-16">
-            <select
-              className="w-full h-full bg-black text-gray-200 text-[32px] rounded-md p-1 pr-10 appearance-none pl-5"
-              value={selectedCountry}
-              onChange={handleCountryChange}
-            >
-              <option value="대한민국">대한민국</option>
-              <option value="USA">USA</option>
-            </select>
-
-            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3 text-gray-200">
-              <svg className="h-10 w-10 fill-current" viewBox="0 0 20 20">
-                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-              </svg>
-            </div>
-          </div>
-        </div>
-
-        <div className="border-t border-gray-400 border-[1px] w-full" />
-
-        <div className="flex px-4 items-center h-[170px] justify-between">
-          <p className="text-[42px]">OEM</p>
-
-          <div className="relative w-60 h-16">
-            <select
-              className="w-full h-full bg-black text-gray-200 text-[32px] rounded-md p-1 pr-10 appearance-none pl-5"
-              value={selectedOEM}
-              onChange={handleOEMChange}
-            >
-              <option value="Hyundai">Hyundai</option>
-              <option value="RKM">RKM</option>
-              <option value="TOYOTA">TOYOTA</option>
-              <option value="KGM">KGM</option>
-              <option value="Kia">Kia</option>
-            </select>
-
-            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3 text-gray-200">
-              <svg className="h-10 w-10 fill-current" viewBox="0 0 20 20">
-                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-              </svg>
-            </div>
-          </div>
-        </div> */}
-
-        {/* <div className="border-t border-gray-400 border-[1px] w-full" />
-
-        <div className="flex px-4 items-center h-[170px] justify-between">
-          <p className="text-[42px]">Live</p>
-          {isLiveVersion ? (
-            <PiToggleRightFill size={80} className="cursor-pointer" onClick={toggleLiveVersion} />
-          ) : (
-            <PiToggleLeftLight size={80} className="cursor-pointer" onClick={toggleLiveVersion} />
-          )}
-        </div>
-
-        <div className="border-t border-gray-400 border-[1px] w-full" />
-
-        <div className="flex px-4 items-center h-[170px] justify-between">
-          <p className="text-[42px]">라디오</p>
-          {isRadioVersion ? (
-            <PiToggleRightFill size={80} className="cursor-pointer" onClick={toggleRadioVersion} />
-          ) : (
-            <PiToggleLeftLight size={80} className="cursor-pointer" onClick={toggleRadioVersion} />
-          )}
-        </div> */}
       </div>
     </div>
   );
