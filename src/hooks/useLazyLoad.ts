c7977574fc4666b286169b0492c89e5987c6fc6e
Ethan <Ethan.Jung@obigo.com>
2025-12-01 16:47:06 +0900

    feat:useLazyLoad 훅 생성

---
+++ src/hooks/useLazyLoad.ts
@@ -0,0 +1,40 @@
+import { useEffect, useRef, useState } from 'react';
+
+interface UseLazyLoadOptions {
+  rootMargin?: string;
+  threshold?: number;
+  priority?: boolean;
+}
+
+export function useLazyLoad(options?: UseLazyLoadOptions) {
+  const [isInView, setIsInView] = useState(options?.priority || false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (options?.priority) return;
+
+    const element = ref.current;
+    if (!element) return;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsInView(true);
+          observer.disconnect();
+        }
+      },
+      {
+        rootMargin: options?.rootMargin || '200px',
+        threshold: options?.threshold || 0.01,
+      }
+    );
+
+    observer.observe(element);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [options?.priority, options?.rootMargin, options?.threshold]);
+
+  return { ref, isInView };
+}
