1628539b9cf56aa28df35b13aaee1818ed00176c
Ethan <Ethan.Jung@obigo.com>
2025-12-04 16:57:35 +0900

    feat: 영문 전환 기능

--- src/hooks/useSection.ts
+++ src/hooks/useSection.ts
@@ -1,17 +1,23 @@
 import { useQuery } from '@tanstack/react-query';
+import { useTranslation } from 'react-i18next';
 import { supabase } from '../lib/supabaseClient';
 import type { SectionItemType } from '../types/section';
 
 export function useSection(sectionId: number, selectedOEM?: string) {
+  const { i18n } = useTranslation();
+  const currentLanguage = i18n.language.startsWith('en') ? 'en' : 'ko';
+  console.log(i18n.language);
+
   const {
     data = [],
     isLoading,
     error,
   } = useQuery<SectionItemType[]>({
-    queryKey: ['section', sectionId, selectedOEM],
+    queryKey: ['section', sectionId, selectedOEM, currentLanguage],
     queryFn: async () => {
       const { data, error } = await supabase.rpc('get_section_items', {
         section_id: sectionId,
+        language: currentLanguage,
       });
 
       if (error) {
