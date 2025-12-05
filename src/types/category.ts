4d71e2cd2869b703538d65b8546630239cfb0521
Jennifer <jennifer.choi@obigo.com>
2025-12-05 11:37:40 +0900

    refactor: 언어에 따라 카테고리 이미지 및 제목 다르게 뜨도록 수정

--- src/types/category.ts
+++ src/types/category.ts
@@ -3,10 +3,10 @@ import type { ProgramType } from './program';
 export type CategoryType = {
   id: number;
   title: string;
-  en_title: string;
   img_url: string;
   order: string;
   created_at: string;
   type: 'radio' | 'podcast';
   programs: ProgramType[];
+  language: string;
 };
